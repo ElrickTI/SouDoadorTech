5e688a752087a348fc1c96e8072991a3ce44e264
Elrick Junior <elrickfernando60@gmail.com>
2025-03-07 15:05:01 -0300

    Criação da Entidade Usuario

--- BACK-END/SouDoadorTECH/src/main/java/com/SouDoadorTECH/models/Usuario.java
+++ BACK-END/SouDoadorTECH/src/main/java/com/SouDoadorTECH/models/Usuario.java
@@ -1,6 +1,76 @@
 package com.SouDoadorTECH.models;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+import java.util.Objects;
+
+@Entity
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idUsuario;
+    private String nomeCompleto;
+    private String usuario;
+    private String senha;
+    private String email;
+
+
+    public Usuario(){}
+
+    public Long getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Long idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return Objects.equals(idUsuario, usuario.idUsuario);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(idUsuario);
+    }
 }
